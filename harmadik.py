0c106555424fefe5f8bbd761a1d3ef73615bc315
RCsSto <riazancevcsanad@turr.hu>
2022-03-16 11:20:44 +0100

    három mező elkészítve

--- harmadik.py
+++ harmadik.py
@@ -7,4 +7,18 @@ cimke1.pack(side = "left")
 mezo1 = Entry(foablak)
 mezo1.pack(side = "left") 
 
+cimke2 = Label(foablak, text = " ")
+cimke2.pack(side = "left")
+
+cimke3 = Label(foablak, text = "Második mező")
+mezo2 = Entry(foablak)
+mezo2.pack(side = "left") 
+
+cimke4 = Label(foablak, text = " ")
+cimke2.pack(side = "left")
+
+cimke5 = Label(foablak, text = "Harmadik mező")
+mezo3 = Entry(foablak)
+mezo3.pack(side = "left") 
+
 foablak.mainloop()
