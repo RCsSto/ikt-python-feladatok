a439beced3144c822af0cb7afaf997202cfc14b2
RCsSto <riazancevcsanad@turr.hu>
2022-04-04 13:22:21 +0200

    Kiszámítás befejezése

--- Feladat-2/folyadek.py
+++ Feladat-2/folyadek.py
@@ -6,36 +6,30 @@ foablak = Tk()
 
 #def
 def liter():
-    a = int(mezo1.get())
-    b = int(mezo2.get())
-    c = int(mezo3.get())
+    m = int(mezo2.get()) 
+    r = int(mezo3.get())
+    mennyiseg = int(mezo7.get())
 
-    N = math.pi * b * b * c
-    R = N / 100
+    terfogat = math.pi * (m * m) * r
 
-
-    M = d / a
     mezo5.delete(0, END)
-    mezo5.insert(0, str(s)+" bor")
+    mezo5.insert(0, str(mennyiseg)+" liter borunk van")
     mezo6.delete(0, END)
-    mezo6.insert(0, str()+" igen")
+    if mennyiseg <= 100:
+        mezo6.insert(0, str()+" Belefér-e? Igen")
+    else:
+        mezo6.insert(0, str()+" Belefér-e? Nem")
 
     mezo4.delete(0, END)
-    mezo4.insert(0, str(PLNRW)+" liter")
+    mezo4.insert(0, str(terfogat)  +" a hordó térfogata.")
 
 #Kód
 
 szoveg = Label(foablak, text = "Van egy henger alakú hordónk, melybe nem tudjuk, hogy belefér-e a rendelkezésre álló bor.")
 szoveg.grid(row = 0, column = 0)
 
-cimke = Label(foablak, text = "Bor mennyiség: ")
-cimke.grid(row = 1, column = 0)
-
-mezo1 = Entry(foablak)
-mezo1.grid(row = 1, column = 1)
-
 elval = Label(foablak, text = " ")
-elval.grid(row = 1, column = 2)
+elval.grid(row = 1, column = 1)
 
 elval1 = Label(foablak, text = " ")
 elval1.grid(row = 2, column = 0)
@@ -49,7 +43,7 @@ mezo2.grid(row = 3, column = 1)
 elval2 = Label(foablak, text = " ")
 elval2.grid(row = 4, column = 0)
 
-cimke2 = Label(foablak, text = "Mi a hordó sugara?")
+cimke2 = Label(foablak, text = "Hordó sugara: ")
 cimke2.grid(row = 5, column = 0)
 
 mezo3 = Entry(foablak)
@@ -75,5 +69,11 @@ mezo5.grid(row = 8, column = 3)
 
 mezo6 = Entry(foablak)
 mezo6.grid(row = 8, column = 5)
+
+cimke4 = Label(foablak, text = "A hordóba ennyi folyadék fér: ")
+cimke4.grid(row = 2, column = 0)
+
+mezo7 = Entry(foablak)
+mezo7.grid(row = 2, column = 1)
 
 foablak.mainloop()
