6c4b1841fe88a6fc1b86a94db3f480b9530b2d47
RCsSto <riazancevcsanad@turr.hu>
2022-04-25 17:56:13 +0200

    Teljes kód (A githubom vicceskedik és törölte az eddig kész lévő kódot)

--- Menüsor/main.py
+++ Menüsor/main.py
@@ -2,6 +2,121 @@
 
 from tkinter import * 
 
+# Fontosabb Def-ek
+
+def nevjegy():
+    ablak_egy = Toplevel(foablak)
+    uzenet_egy = Message(ablak_egy, text = "Készítette: Riazáncev Csanád \n Baja \n 2022.04.20", width = 300)
+    gomb_egy = Button(ablak_egy, text = "Kilépés", command = ablak_egy.destroy)
+
+    uzenet_egy.pack()
+    gomb_egy.pack()
+
+    ablak_egy.mainloop()
+
+# Def és Widgetek és Mellékablakok
+
+    # Felszín 
+
+def felszin():
+    def szamit():
+        a = eval(mezo_egy.get())
+        b = eval(mezo_ketto.get())
+        c = eval(mezo_harom.get())
+
+        felszin = 2 * (a * b + a * c + b * c)
+
+        if felszin < 0 and type(felszin) == str:
+            mezo_negy.delete(0, END)
+            mezo_negy.insert(0, str("A szám kisebb mint 0, vagy betű szerepel benne!"))
+        else:
+            mezo_negy.delete(0, END)
+            mezo_negy.insert(0, str(felszin))
+
+    ablak_ketto = Toplevel(foablak)
+    ablak_ketto.title("A téglatest felszíne")
+    ablak_ketto.minsize(width = 300, height = 300)
+
+    szoveg_egy = Label(ablak_ketto, text = "a")
+    szoveg_ketto = Label(ablak_ketto, text = "b")
+    szoveg_harom = Label(ablak_ketto, text = "c")
+    szoveg_negy = Label(ablak_ketto, text = "Eredmény: ")
+
+    gomb_ketto = Button(ablak_ketto, text = "Kiszámítás", command = szamit)
+
+    mezo_egy = Entry(ablak_ketto)
+    mezo_ketto = Entry(ablak_ketto)
+    mezo_harom = Entry(ablak_ketto)
+    mezo_negy = Entry(ablak_ketto)
+
+        # Poziciónálás 
+            # Felszín
+
+    szoveg_egy.grid(row = 1)
+    szoveg_ketto.grid(row = 2)
+    szoveg_harom.grid(row = 3)
+    szoveg_negy.grid(row = 4)
+
+    gomb_ketto.grid(row = 4, column = 2, sticky = W)
+
+    mezo_egy.grid(row = 1, column = 2, sticky = W)
+    mezo_ketto.grid(row = 2, column = 2, sticky = W)
+    mezo_harom.grid(row = 3, column = 2, sticky = W)
+    mezo_negy.grid(row = 5, column = 2, sticky = W)
+
+    ablak_ketto.mainloop()
+
+    # Térfogat
+
+def terfogat():
+    def szamit():
+        a = eval(mezo_egy.get())
+        b = eval(mezo_ketto.get())
+        c = eval(mezo_harom.get())
+
+        terfogat = a * b * c
+
+        if terfogat < 0 and type(felszin) == str:
+            mezo_negy.delete(0, END)
+            mezo_negy.insert(0, str("A szám kisebb mint 0, vagy betű szerepel benne!"))
+        else:
+            mezo_negy.delete(0, END)
+            mezo_negy.insert(0, str(terfogat))
+
+    ablak_ketto = Toplevel(foablak)
+    ablak_ketto.title("A téglatest térfogata")
+    ablak_ketto.minsize(width = 300, height = 300)
+
+    szoveg_egy = Label(ablak_ketto, text = "a")
+    szoveg_ketto = Label(ablak_ketto, text = "b")
+    szoveg_harom = Label(ablak_ketto, text = "c")
+    szoveg_negy = Label(ablak_ketto, text = "Eredmény: ")
+
+    gomb_ketto = Button(ablak_ketto, text = "Kiszámítás", command = szamit)
+
+    mezo_egy = Entry(ablak_ketto)
+    mezo_ketto = Entry(ablak_ketto)
+    mezo_harom = Entry(ablak_ketto)
+    mezo_negy = Entry(ablak_ketto)
+
+
+        # Pozicónálás
+            #Térfogat
+
+    szoveg_egy.grid(row = 1)
+    szoveg_ketto.grid(row = 2)
+    szoveg_harom.grid(row = 3)
+    szoveg_negy.grid(row = 4)
+
+    gomb_ketto.grid(row = 4, column = 2, sticky = W)
+
+    mezo_egy.grid(row = 1, column = 2, sticky = W)
+    mezo_ketto.grid(row = 2, column = 2, sticky = W)
+    mezo_harom.grid(row = 3, column = 2, sticky = W)
+    mezo_negy.grid(row = 5, column = 2, sticky = W)
+
+    ablak_ketto.mainloop()
+
 # Foablak 
 
 foablak = Tk()
@@ -13,7 +128,7 @@ foablak.minsize(width = 300, height = 100)
 menusor = Frame(foablak)
 menusor.pack(side = TOP, fill = X)
 
-menu_egy = Menubutton(menusor, text = "File", underline = 0)
+menu_egy = Menubutton(menusor, text = "Fájl", underline = 0)
 menu_egy.pack(side = LEFT)
 
 file = Menu(menu_egy)
@@ -29,11 +144,6 @@ teglatest.add_command(label = "Felszín", command = felszin, underline = 0)
 teglatest.add_command(label = "Térfogat", command = terfogat, underline = 0)
 menu_ketto.config(menu = teglatest)
 
-# Def
-def nevjegy():
-    ablak_egy = Toplevel(foablak)
-
-
 # Mainloop
 
-foablak.mainloop
+foablak.mainloop()
